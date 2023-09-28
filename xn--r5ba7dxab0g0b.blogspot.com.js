d485485df233453498e369c17693992a1189fd4b
aufftool <129205104+aufftool@users.noreply.github.com>
2023-09-28 08:09:43 +0530

    Create xn--r5ba7dxab0g0b.blogspot.com.js

---
+++ xn--r5ba7dxab0g0b.blogspot.com.js
@@ -0,0 +1,10 @@
+
+	atOptions = {
+		'key' : '4d3cfb281126dfdcaa3cd43937bf274c',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="//www.profitablecreativeformat.com/4d3cfb281126dfdcaa3cd43937bf274c/invoke.js"></scr' + 'ipt>');
+//ads
