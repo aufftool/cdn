e02985d4c7a0c1c5c53bb3857af9565522dace22
aufftool <129205104+aufftool@users.noreply.github.com>
2024-05-16 21:30:30 +0530

    Create AIzaSyDAcaf7DvzHxp3poJzC47nlnTiwP0Ha2Rk.js

---
+++ AIzaSyDAcaf7DvzHxp3poJzC47nlnTiwP0Ha2Rk.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDAcaf7DvzHxp3poJzC47nlnTiwP0Ha2Rk",
+  authDomain: "madhav-whiteactionyt.firebaseapp.com",
+  projectId: "madhav-whiteactionyt",
+  storageBucket: "madhav-whiteactionyt.appspot.com",
+  messagingSenderId: "198109045455",
+  appId: "1:198109045455:web:d5fe0363852b9cb0a80dba",
+  measurementId: "G-X80BM5D2Z5"
+};
+firebase.initializeApp(firebaseConfig);
