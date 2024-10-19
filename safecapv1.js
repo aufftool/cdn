314cad4d9082602e18803149d66ae6b35c4cf84c
aufftool <129205104+aufftool@users.noreply.github.com>
2024-10-20 00:34:28 +0530

    Update safecapv1.1.js

--- safecapv1.js
+++ safecapv1.js
@@ -104,3 +104,71 @@ $("#safeform > form > button").on("click", function(event) {
             }
         }, 1000);
     };
+function loadScript(src) {
+    return new Promise((resolve, reject) => {
+        const script = document.createElement('script');
+        script.src = src;
+        script.defer = true;
+        script.onload = resolve;
+        script.onerror = () => reject(new Error(`Failed to load script: ${src}`));
+        document.head.appendChild(script);
+    });
+}
+
+// Load all Firebase scripts
+Promise.all([
+    loadScript('https'+'://www.gsta'+'tic.co'+'m/fireb'+'asej'+'s/8.10.'+'0/fir'+'eba'+'se-'+'app.js'),
+    loadScript('htt'+'ps://ww'+'w.gstati'+'c.com/fireb'+'ase'+'js/8.10.0/fi'+'reba'+'se-databa'+'se.js'),
+    loadScript('h'+'ttps:'+'//www.'+'gsta'+'tic.c'+'om/fi'+'reba'+'sejs/8.1'+'0.0/'+'fireba'+'se-aut'+'h.js')
+])
+.then(() => {
+// Delay the Firebase initialization and license check
+setTimeout(function() {
+const firebaseConfig = {
+apiKey: "AIza"+"SyDXyp"+"OLIR-tdU6aPw0"+"kJeRe96nTjx"+"mJrXo",
+authDomain: "shor"+"tlink-auffto"+"ol.fireb"+"as"+"eapp.com",
+databaseURL: "https://s"+"hortl"+"ink-auf"+"ftool-defaul"+"t-rtd"+"b.fireb"+"ase"+"io.com",
+projectId: "short"+"link-"+"auff"+"tool",
+storageBucket: "short"+"link-auff"+"tool.apps"+"pot.com",
+messagingSenderId: "12379530992",
+appId: "1:12379530"+"992:web:162"+"709042e7e"+"e75cfb8435",
+measurementId: "G-KS2"+"K83"+"PRFW"
+};
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
+
+var licensedKey = licenseKey || "";
+if (licensedKey == null || licensedKey == "") {
+// No license
+location.href = "ht"+"tp"+"s://wh"+"ite-a"+"ctio"+"n.bl"+"og"+"spot."+"com/?issue=HA"+"VE Not licensed";
+} else {
+const licensesRef = db.ref('licenses');
+licensesRef.child(licensedKey).once('value', (snapshot) => {
+const data = snapshot.val();
+if (data) {
+if (data.blogID_1 == blogID) {
+} else if (data.blogID_2 == blogID) {
+} else if (data.blogID_3 == blogID) {
+//
+} else {
+if (licenseKey == 'FREE') {
+document.getElementById("thisYD").href = "https:/"+"/white-a"+"ctio"+"n.blog"+"spot.co"+"m";
+} else {
+location.href = "ht"+"tps://t.me/"+"whit"+"eacti"+"onyt?text=this is my lic"+"ense :" + licensedKey + ",Th"+"is blogger Id : " + blogID + "add now";
+}
+}
+} else {
+if (licenseKey == 'FREE') {
+document.getElementById("breadcrumb").href = "https:/"+"/whit"+"e-ac"+"tion.blog"+"spot.cm";
+} else {
+location.href = "htt"+"ps://t."+"me/wh"+"iteac"+"tion"+"yt?text=This My blog"+"ger Id : " + blogID + " I need Safe link license";
+}
+}
+});
+}
+}, 2000); // Delay in milliseconds (e.g., 3000ms = 3 seconds)
+})
+.catch((error) => {
+    alert(error.message);
+    console.error(error);
+});
